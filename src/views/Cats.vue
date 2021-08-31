3a597dae6c2c4e00eff4af405e75a40d8fbc81a8
byron <broby@IDM-PC-0066.IDM.Local>
2021-08-31 15:34:39 -0400

    New Vue adopt pets project

---
+++ src/views/Cats.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-table striped hover :items="items"/>
+    </div>
+</template>
+
+<script>
+// @ is a shortcut that vue cli sets up for you, its a reference to the 'src' directory
+// so that you don't have to do ../../ and so on to get back to the 'src' directory
+import cats from '@/data/cats'
+
+export default {
+    data() {
+        return {
+        items: [
+            // cats is equivlant to cats: cats (where the first 'cats' is the variable 
+            // we're declaring and the second cats value is imported cats array)
+            cats
+        ]
+        }
+    }
+}
+</script>
